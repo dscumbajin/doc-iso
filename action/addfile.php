1d4afdbb222b8fd36002f45cb6a0e00fea5f6227
dscumbajin <dscumbajin@uce.edu.ec>
2021-04-12 16:21:25 -0500

    actual

--- action/addfile.php
+++ action/addfile.php
@@ -1,58 +1,56 @@
 <?php
 
-	session_start();
+session_start();
 
-	include "../config/config.php";
-	include "class.upload.php";
+include "../config/config.php";
+include "class.upload.php";
 
-	//print_r($_SESSION);
-if(!empty($_POST) && isset($_SESSION["user_id"])){
+//print_r($_SESSION);
+if (!empty($_POST) && isset($_SESSION["user_id"])) {
 
-	$alphabeth ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWYZ1234567890_-";
+	$alphabeth = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWYZ1234567890_-";
 	$code = "";
-	for($i=0;$i<12;$i++){
-	    $code .= $alphabeth[rand(0,strlen($alphabeth)-1)];
+	for ($i = 0; $i < 12; $i++) {
+		$code .= $alphabeth[rand(0, strlen($alphabeth) - 1)];
 	}
 
-	$code= $code;
-	$is_public = isset($_POST["is_public"])?1:0;
-	$folder_id = intval($_POST["folder_id"])!="" ? intval($_POST["folder_id"]):"NULL";
+	$code = $code;
+	$is_public = isset($_POST["is_public"]) ? 1 : 0;
+	$folder_id = intval($_POST["folder_id"]) != "" ? intval($_POST["folder_id"]) : "NULL";
 	$folder_id;
 
-	$user_id=$_SESSION["user_id"];
-	$description = mysqli_real_escape_string($con,(strip_tags($_POST["description"],ENT_QUOTES)));
+	$user_id = $_SESSION["user_id"];
+	$description = mysqli_real_escape_string($con, (strip_tags($_POST["description"], ENT_QUOTES)));
 	$created_at = "NOW()";
 
 
 	$handle = new Upload($_FILES['filename']);
-	
+
 	if ($handle->uploaded) {
-		$url="../storage/data/".$_SESSION["user_id"];
+		$url = "../storage/data/" . $_SESSION["user_id"];
 		$handle->Process($url);
-		if($handle->processed){
-	    $filename = $handle->file_dst_name;
+		if ($handle->processed) {
+			$filename = $handle->file_dst_name;
 
-		$sql = "INSERT INTO file (code, filename, description, is_public, user_id, is_folder, folder_id, created_at) VALUES (\"$code\",\"$filename\",\"$description\", $is_public, $user_id, 0, $folder_id, NOW());";
+			$sql = "INSERT INTO file (code, filename, description, is_public, user_id, is_folder, folder_id, created_at) VALUES (\"$code\",\"$filename\",\"$description\", $is_public, $user_id, 0, $folder_id, NOW());";
 
-		$query=mysqli_query($con, $sql);
-		if ($query) {
-			// echo "archivo agregado con exito";
-			header("location: ../newfile?success");
-		}else{
-			// echo "no se pudo, subir hubo un error".mysqli_error($con)."<br>.".mysqli_errno($con);
-			header("location: ../newfile?error");
-		}
-
-		}else{
+			$query = mysqli_query($con, $sql);
+			if ($query) {
+				// echo "archivo agregado con exito";
+				header("location: ../newfile?success");
+			} else {
+				// echo "no se pudo, subir hubo un error".mysqli_error($con)."<br>.".mysqli_errno($con);
+				header("location: ../newfile?error");
+			}
+		} else {
 			// echo "el archivo no se subio por peso maximo";
 			header("location: ../newfile?error2&max_size");
 		}
-
-	}else{
-			// echo "el archivo no se subio por peso maximo 2";
-			header("location: ../newfile?error3&fatal");
+	} else {
+		// echo "el archivo no se subio por peso maximo 2";
+		header("location: ../newfile?error3&fatal");
 	}
-}else{
+} else {
 	//echo "aas";
 	header("location: ../");
 }
--- newfolder.php
+++ newfolder.php
@@ -1,72 +1,80 @@
-<?php 
+<?php
 
-    $active4="active"; 
-    include "header.php"; 
-    
- ?>
-    
-    <div class="content-wrapper"><!-- Content Wrapper. Contains page content -->
-        <section class="content-header"><!-- Content Header (Page header) -->
-            <h1>Nueva Carpeta</h1>
-            <ol class="breadcrumb">
-                <li><a href="home"><i class="fa fa-dashboard"></i> Home</a></li>
-                <li><a href="myfiles"><i class="fa fa-archive"></i> Mis Archivos</a></li>
-                <li class="active">Nueva Carpeta</li>
-            </ol>
-        </section>
-        <section class="content"><!-- Main content -->
-            <div class="row"><!-- Small boxes (Stat box) -->
-                <div class="col-md-6 col-md-offset-3">
-                    <?php
-                        // get messages
-                        if (isset($_GET['success'])) {
-                            echo "<p class='alert alert-success'> <i class=' fa fa-exclamation-circle'></i> <strong>¡Bien hecho! </strong> Carpeta creada satisfactioramente.</p>";
-                        }elseif(isset($_GET['error'])) {
-                             echo "<p class='alert alert-danger'> <i class=' fa fa-exclamation-circle'></i> <strong>¡Error! </strong>No se pudo crear la carpeta.</p>";
-                        }
-                    ?>
-                    <div class="box box-warning"><!-- general form elements -->
-                        <div class="box-header with-border">
-                            <h3 class="box-title"><button class="btn btn-xs btn-success"><i class="fa fa-plus"></i></button> Nueva Carpeta</h3>
-                        </div><!-- /.box-header -->
-                        <form role="form" action="action/addfolder.php" method="post"><!-- form start -->
-                            <div class="box-body">
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1">Ubicacion</label>
-                                    <select class="form-control select2" style="width: 100%;" name="folder_id">
-                                        <option value="">Mi unidad</option>
-                                        <?php 
-                                            function getfolders($id,$lev){
-                                                global $con;
-                                                $folder = mysqli_query($con, "select * from file where id=$id");
-                                                $folder_rw = mysqli_fetch_array($folder);
-                                                $str = str_repeat("+", $lev);
-                                                echo "<option value='".$folder_rw['id']."'>".$str.$folder_rw['filename'].'/'. $folder_rw['description']."</option>";
+$active4 = "active";
+include "header.php";
 
-                                                $folder_id=$folder_rw['id'];
-                                                $getFoldersByFolderId=mysqli_query($con, "select * from file where folder_id=$folder_id and is_folder=1 and status=1 order by created_at desc");
-                                                foreach($getFoldersByFolderId as $f){
-                                                    getfolders($f['id'],$lev+1);
-                                                }
-                                            }
-                                            $user_id=$_SESSION["user_id"];
-                                            $getRootFoldersByUserId=mysqli_query($con, "select * from file where user_id=$user_id and is_folder=1 and folder_id is NULL and status=1 order by created_at desc");
-                                            foreach($getRootFoldersByUserId as $f):
-                                                getfolders($f['id'],0);
-                                            endforeach;
-                                        ?>
-                                    </select>
-                                </div>
-                                <div class="form-group">
-                                    <label for="name_folder">Nombre</label>
-                                    <input type="text" name="filename" class="form-control" id="name_folder" placeholder="Nombre de la carpeta">
-                                </div>
-                                <div class="form-group">
-                                    <label>Descripción</label>
-                                    <textarea class="form-control" name="description" rows="3" placeholder="Descripción ..."></textarea>
-                                </div>
+?>
 
-                                <?php if($is_evaluator == 1):?>
+<div class="content-wrapper">
+    <!-- Content Wrapper. Contains page content -->
+    <section class="content-header">
+        <!-- Content Header (Page header) -->
+        <h1>Nueva Carpeta</h1>
+        <ol class="breadcrumb">
+            <li><a href="home"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li><a href="myfiles"><i class="fa fa-archive"></i> Mis Archivos</a></li>
+            <li class="active">Nueva Carpeta</li>
+        </ol>
+    </section>
+    <section class="content">
+        <!-- Main content -->
+        <div class="row">
+            <!-- Small boxes (Stat box) -->
+            <div class="col-md-6 col-md-offset-3">
+                <?php
+                // get messages
+                if (isset($_GET['success'])) {
+                    echo "<p class='alert alert-success'> <i class=' fa fa-exclamation-circle'></i> <strong>¡Bien hecho! </strong> Carpeta creada satisfactioramente.</p>";
+                } elseif (isset($_GET['error'])) {
+                    echo "<p class='alert alert-danger'> <i class=' fa fa-exclamation-circle'></i> <strong>¡Error! </strong>No se pudo crear la carpeta.</p>";
+                }
+                ?>
+                <div class="box box-warning">
+                    <!-- general form elements -->
+                    <div class="box-header with-border">
+                        <h3 class="box-title"><button class="btn btn-xs btn-success"><i class="fa fa-plus"></i></button> Nueva Carpeta</h3>
+                    </div><!-- /.box-header -->
+                    <form role="form" action="action/addfolder.php" method="post">
+                        <!-- form start -->
+                        <div class="box-body">
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Ubicacion</label>
+                                <select class="form-control select2" style="width: 100%;" name="folder_id">
+                                    <option value="">Mi unidad</option>
+                                    <?php
+                                    function getfolders($id, $lev)
+                                    {
+                                        global $con;
+                                        $folder = mysqli_query($con, "select * from file where id=$id");
+                                        $folder_rw = mysqli_fetch_array($folder);
+                                        $str = str_repeat("+", $lev);
+                                        echo "<option value='" . $folder_rw['id'] . "'>" . $str . $folder_rw['filename'] . '/' . $folder_rw['description'] . "</option>";
+                                        //echo "<option value='".$folder_rw['id']."'>". $folder_rw['description'].'/'.$folder_rw['filename']."</option>";
+
+                                        $folder_id = $folder_rw['id'];
+                                        $getFoldersByFolderId = mysqli_query($con, "select * from file where folder_id=$folder_id and is_folder=1 and status=1 order by created_at desc");
+                                        foreach ($getFoldersByFolderId as $f) {
+                                            getfolders($f['id'], $lev + 1);
+                                        }
+                                    }
+                                    $user_id = $_SESSION["user_id"];
+                                    $getRootFoldersByUserId = mysqli_query($con, "select * from file where user_id=$user_id and is_folder=1 and folder_id is NULL and status=1 order by created_at desc");
+                                    foreach ($getRootFoldersByUserId as $f) :
+                                        getfolders($f['id'], 0);
+                                    endforeach;
+                                    ?>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="name_folder">Nombre</label>
+                                <input type="text" name="filename" class="form-control" id="name_folder" placeholder="Nombre de la carpeta">
+                            </div>
+                            <div class="form-group">
+                                <label>Descripción</label>
+                                <textarea class="form-control" name="description" rows="3" placeholder="Descripción ..."></textarea>
+                            </div>
+
+                            <?php if ($is_evaluator == 1) : ?>
                                 <div class="form-group">
                                     <div class="checkbox icheck">
                                         <label>
@@ -74,16 +82,16 @@
                                         </label>
                                     </div>
                                 </div>
-                                <?php endif;?>
-                            </div><!-- /.box-body -->
-                            <div class="box-footer">
-                                <button type="submit" class="btn btn-warning">Crear Carpeta</button>
-                            </div>
-                        </form>
-                    </div><!-- /.box -->
-                </div>
-            </div><!-- /.row -->
-        </section>
-    </div><!-- /.content -->
+                            <?php endif; ?>
+                        </div><!-- /.box-body -->
+                        <div class="box-footer">
+                            <button type="submit" class="btn btn-warning">Crear Carpeta</button>
+                        </div>
+                    </form>
+                </div><!-- /.box -->
+            </div>
+        </div><!-- /.row -->
+    </section>
+</div><!-- /.content -->
 
 <?php include "footer.php"; ?>
